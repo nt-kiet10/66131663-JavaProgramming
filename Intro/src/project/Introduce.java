275d6395b16cbc8911a3c6eadf3290236a7569a2
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-02-05 23:53:00 +0700

    Intro
    tạo project

---
+++ Intro/src/project/Introduce.java
@@ -0,0 +1,10 @@
+package project;
+
+public class Introduce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Xin chao toi la sinh vien khoa CNTT");
+	}
+
+}
