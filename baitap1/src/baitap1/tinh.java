ff5236df38e50961fd1ce7ab2bbc177eb4a873a0
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-02-06 00:26:55 +0700

    tinhtoan

---
+++ baitap1/src/baitap1/tinh.java
@@ -0,0 +1,22 @@
+
+
+package baitap1;
+
+public class tinh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 30;
+        int b = 7;
+        double c = 4.5;
+        
+        int tong = a + b;
+        double thuong = a/c;
+        int phandu = a%(int)c;
+        
+        System.out.println("Tổng : "+ tong);
+        System.out.println("Thương : "+ thuong);
+        System.out.println("Phần dư : "+ phandu);
+	}
+       
+}
