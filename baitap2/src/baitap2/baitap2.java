80c94e7d053a180a27cfefbf55596d6836136887
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-02-06 00:35:56 +0700

    chuyển đổi ºC -> ºF

---
+++ baitap2/src/baitap2/baitap2.java
@@ -0,0 +1,12 @@
+package baitap2;
+
+public class baitap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      double celsius = 35.0;
+      double fahrenheit = (celsius * 9/5) + 32;
+      System.out.print(celsius+"ºC = "+fahrenheit+"ºF");
+	}
+
+}
