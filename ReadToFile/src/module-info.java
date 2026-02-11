058be47d33a50aa6b6bd9a77ef8e7fc921c23152
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-02-11 22:25:12 +0700

    đọc file

--- ReadToFile/src/module-info.java
+++ ReadToFile/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module ReadToFile {
+module ReadtoFile {
 }
--- ReadToFile/src/readtofile/doctufile.java
+++ ReadToFile/src/readtofile/doctufile.java
@@ -1,27 +1,26 @@
 package readtofile;
 import java.io.BufferedReader;
 import java.io.FileReader;
+
 public class doctufile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		        try {
-		            FileReader fr = new FileReader("input.txt");
-		            BufferedReader br = new BufferedReader(fr);
+		  try {
+	            FileReader fr = new FileReader("input.txt");
+	            BufferedReader br = new BufferedReader(fr);
 
-		            String line;
-		            while ((line = br.readLine()) != null) {
-		                System.out.println(line);
-		            }
+	            String line;
+	            while ((line = br.readLine()) != null) {
+	                System.out.println(line);
+	            }
 
-		            br.close();
-		            fr.close();
-		        } catch (Exception e) {
-		            System.out.println("Lỗi đọc file!");
-		            e.printStackTrace();
-		        }
-		    }
+	            br.close();
+	            fr.close();
+	        } catch (Exception e) {
+	            System.out.println("Lỗi đọc file!");
+	            e.printStackTrace();
+	        }
 	}
 
-		 
-
+}
