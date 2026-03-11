76141776149ff553d6249b8a8298138ccb2d6673
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-03-11 15:45:44 +0700

    update getter - setter

--- vd2_OOP_HàmTạoGetter_Setter/src/vd2_OOP_HàmTạoGetter_Setter/Car.java
+++ vd2_OOP_HàmTạoGetter_Setter/src/vd2_OOP_HàmTạoGetter_Setter/Car.java
@@ -1,18 +1,44 @@
-
 public class Car {
 	// Tạo các thuộc tính cho xe 
 	private String HangXe;
 	private String MauXe;
-	private int tuoi;
-	private double gia;
-	
+	private int Tuoi;
+	private double Gia;
 	//tạo constructor 
-	public Car(String HangXe; String MauXe; int tuoi; double gia ) {
+	public Car(String HangXe, String MauXe, int Tuoi, double Gia ) {
 		this.HangXe = HangXe;
 		this.MauXe = MauXe;
-		this.tuoi = tuoi;
-		this.gia = gia;
+		this.Tuoi = Tuoi;
+		this.Gia = Gia;
 	}
-	
+	//getter 
+	public String getHangXe() {
+		return HangXe;
+	}
+	public String getMauXe() {
+		return MauXe;
+	}
+	public int getTuoi() {
+		return Tuoi;
+	}
+	public double getGia() {
+		return Gia;
+	}
+	//setter
+	public void setHangXe(String HangXe) {
+	    this.HangXe = HangXe;
+	}
+
+	public void setMauXe(String MauXe) {
+	    this.MauXe = MauXe;
+	}
+
+	public void setTuoi(int Tuoi) {
+	    this.Tuoi = Tuoi;
+	}
+
+	public void setGia(double Gia) {
+	    this.Gia = Gia;
+	}
 
 }
