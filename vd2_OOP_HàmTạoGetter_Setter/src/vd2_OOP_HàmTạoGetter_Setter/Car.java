ba56d65a8e13454f909f0c2a3f7edbf06b85124d
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-03-11 15:34:15 +0700

    tạo thuộc tính và constructor

---
+++ vd2_OOP_HàmTạoGetter_Setter/src/vd2_OOP_HàmTạoGetter_Setter/Car.java
@@ -0,0 +1,18 @@
+
+public class Car {
+	// Tạo các thuộc tính cho xe 
+	private String HangXe;
+	private String MauXe;
+	private int tuoi;
+	private double gia;
+	
+	//tạo constructor 
+	public Car(String HangXe; String MauXe; int tuoi; double gia ) {
+		this.HangXe = HangXe;
+		this.MauXe = MauXe;
+		this.tuoi = tuoi;
+		this.gia = gia;
+	}
+	
+
+}
