17e737a35f6fe268651bed972d1ce995fffd5ea8
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-03-26 09:54:34 +0700

    vd_dsHocSinh

--- hocSinh/src/ntu/nguyentuanK/HocSinh.java
+++ hocSinh/src/ntu/nguyentuanK/HocSinh.java
@@ -4,39 +4,41 @@ public class HocSinh {
 
     private String tenHS;
 	private int tuoiHS;
-	private double lopHS;
+	private  String lopHS;
 	//Constructor rong
 	public HocSinh() {
 		
 	}
 	//Constructor day du
-	public HocSinh(String tenHS,int tuoiHS, double lopHS) {
+	public HocSinh(String tenHS,int tuoiHS, String lopHS) {
 		this.tenHS = tenHS;
 		this.tuoiHS = tuoiHS;
 		this.lopHS = lopHS;
 	}
 	//Getter & Setter
+	
+	
 	public String getTenHS() {
 		return tenHS;
 	}
 	public void setTenHS(String tenHS) {
 		this.tenHS = tenHS;
 	}
-	public double getTuoiHS() {
+	public int getTuoiHS() {
 		return tuoiHS;
 	}
 	public void setTuoiHS(int tuoiHS) {
 		this.tuoiHS = tuoiHS;
 	}
-	public double getLopHS() {
+	public String getLopHS() {
 		return lopHS;
 	}
-	public void setLopHS(double lopHS) {
+	public void setLopHS(String lopHS) {
 		this.lopHS = lopHS;
 	}
+	
 	@Override
 	public String toString() {
 		return "HocSinh [tenHS=" + tenHS + ", tuoiHS=" + tuoiHS + ", lopHS=" + lopHS + "]";
 	}
-	
 }
--- hocSinh/src/ntu/nguyentuanK/Main.java
+++ hocSinh/src/ntu/nguyentuanK/Main.java
@@ -1,10 +1,50 @@
 package ntu.nguyentuanK;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		ArrayList<HocSinh> list = new ArrayList<>();
+		//Nhap so luong hoc sinh
+		System.out.print("Nhap so luong hoc sinh: ");
+		int n = Integer.parseInt(sc.nextLine());
+		
+		for(int i=1;i <= n;i++) {
+			System.out.println("Nhap thong tin hoc sinh thu " + i + ":");
+			HocSinh hs = new HocSinh();
+			System.out.print("Nhap ten hoc sinh : ");
+			hs.setTenHS(sc.nextLine());
+			System.out.print("Nhap tuoi hoc sinh : ");
+			hs.setTuoiHS(Integer.parseInt(sc.nextLine()));
+			System.out.print("Nhap lop hoc sinh : ");
+		    hs.setLopHS(sc.nextLine());
+			list.add(hs);
+		}
+			System.out.println("\n===== DANH SACH HOC SINH =====");
+	        for (HocSinh hs : list) {
+	            System.out.println(hs.toString());
+	        }
+        // Thêm 1 học sinh mới
+	        System.out.println("\n===== thêm 1 bạn học sinh mới =====");
+	        HocSinh newHS = new HocSinh();
+			System.out.print("Nhap ten hoc sinh : ");
+			newHS.setTenHS(sc.nextLine());
+			System.out.print("Nhap tuoi hoc sinh : ");
+			newHS.setTuoiHS(Integer.parseInt(sc.nextLine()));
+			System.out.print("Nhap lop hoc sinh : ");
+		    newHS.setLopHS(sc.nextLine());
+			list.add(newHS);
+		//Xoa hoc sinh ten cu the
+		    System.out.println("\n===== xoá 1 học sinh =====");
+		    String tenXoa = sc.nextLine();
+		    boolean daXoa = list.removeIf(hs -> hs.getTenHS().equalsIgnoreCase(tenXoa));
+			if(daXoa) {
+				 System.out.println("Da xoa hoc sinh ten: " + tenXoa);
+			} else {
+			    System.out.println("Khong tim thay hoc sinh ten: " + tenXoa);
+			}
+	        sc.close();
 	}
 
 }
