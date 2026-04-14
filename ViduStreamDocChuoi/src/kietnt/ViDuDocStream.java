e027557f17efddb7422e2571d32842bab2cd4231
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-04-14 23:43:32 +0700

    Đọc file từ chuỗi

--- ViduStreamDocChuoi/src/kietnt/ViDuDocStream.java
+++ ViduStreamDocChuoi/src/kietnt/ViDuDocStream.java
@@ -1,11 +1,25 @@
 package kietnt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ViDuDocStream {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		//Khai báo biến đối tượng InputStreamReader 
+		InputStreamReader ipReader = new InputStreamReader(System.in);
 		
+		//Khai báo biến đối tượng 
+		BufferedReader bufReader = new BufferedReader(ipReader);
 		
-
+		//In ra một phần hướng dẫn để User cần làm gì 
+		System.out.print("Nhập một chuỗi bất kì :");
+		
+		//Đọc dữ liệu từ bàn phím, cất vào biến chuỗi 
+			String lineRead = bufReader.readLine();
+		
+		//In ra dòng đọc được 
+        System.out.print(lineRead);
 	}
-
 }
