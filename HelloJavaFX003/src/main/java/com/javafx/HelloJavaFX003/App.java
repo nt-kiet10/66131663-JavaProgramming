1a0b3c635abe09956f24e936bfd6bcf785802fbd
Nguyễn Tuấn Kiệt <ntk181006@gmail.com>
2026-05-13 20:43:46 +0700

    javafx003

--- HelloJavaFX003/src/main/java/com/javafx/HelloJavaFX003/App.java
+++ HelloJavaFX003/src/main/java/com/javafx/HelloJavaFX003/App.java
@@ -2,6 +2,7 @@ package com.javafx.HelloJavaFX003;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -13,15 +14,18 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage primaryStage) {
+    	Button btn = new Button();
+		//
+		btn.setText("Hello World");
+		// Tạo màn hình hiển thị 
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		Scene scene = new Scene(root, 300, 250);
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scene);
+		primaryStage.show();    }
 
     public static void main(String[] args) {
         launch();
